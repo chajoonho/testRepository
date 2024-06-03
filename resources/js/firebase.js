a37b302845de40099c9b07994a3123058addc10f
chajoonho <167054625+chajoonho@users.noreply.github.com>
2024-06-03 17:29:23 +0900

    20240603

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -3,7 +3,13 @@ import {
   getFirestore,
   getDocs,
   collection,
-} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+  setDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBkG-siFj9Egphst4f50AH40t-zZ0PVNAU",
@@ -12,17 +18,48 @@ const firebaseConfig = {
   storageBucket: "myproject-d5035.appspot.com",
   messagingSenderId: "562394154582",
   appId: "1:562394154582:web:fe1c1633caa548cb215df7",
+  measurementId: "G-L8E4XS1JW2",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function getMembers() {
-  const collect = await collection(db, "member");
+async function getDatas(collectionName) {
+  const collect = await collection(db, collectionName);
   const snapshot = await getDocs(collect);
 
   return snapshot;
 }
 
-export { db, getMembers };
+async function addDatas(collectionName, dataObj) {
+  try {
+    // 문서 ID 수동
+    // const saveDoc = await doc(db, collectionName, '3');
+    // console.log(`doc() 결과: ${saveDoc}`);
+    // const saveResult = await setDoc(saveDoc, dataObj);
+    // console.log(`setDoc() 결과: ${saveResult}`);
+
+    // 문서 ID 자동
+    const collect = await collection(db, collectionName);
+    await addDoc(collect, dataObj); // 결과 == undefined
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  await deleteDoc(docRef);
+}
+
+async function updateDatas(collectionName, docId, updateDoc) {
+  // doc(db, 컬렉션명, 문서ID);
+  // getDocs(문서레퍼런스);
+  // updateDoc(문서데이터, 수정할정보);
+  const docRef = await doc(db, collectionName, docId);
+  const docData = await getDocs(docRef);
+  await updateDoc(docData, updateInfoObj);
+}
+
+export { db, getDatas, addDatas, deleteDatas };
