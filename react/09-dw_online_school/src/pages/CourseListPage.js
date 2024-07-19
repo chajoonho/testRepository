e522356dbb5e30ea5f8f0a25f42bb47355162f60
chajoonho <wnsgh882002@gmail.com>
2024-07-19 17:29:06 +0900

    20240719

--- react/09-dw_online_school/src/pages/CourseListPage.js
+++ react/09-dw_online_school/src/pages/CourseListPage.js
@@ -4,12 +4,14 @@ import searchImg from "../assets/search.svg";
 import styles from "./CourseListPage.module.css";
 import CourseItem from "../components/CourseItem";
 import { getDatas } from "../api/firebase";
+import Warn from "../components/Warn";
 
 let listItems;
 
 function CourseListPage(props) {
   const [items, setItems] = useState([]);
   const [keyword, setKeyword] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleKeywordChange = (e) => {
     // 사용자가 입력한 키워드를 state에 저장한다.
@@ -30,12 +32,14 @@ function CourseListPage(props) {
   };
 
   const handleLoad = async () => {
+    setIsLoading(true);
     // 파이어베이스의 courses 컬렉션의 데이터를 가져온다.
     const resultData = await getDatas("courses");
     // 가져온 데이터 콘솔로 확인.
     listItems = resultData;
     // items state에 set 해준다.
     setItems(resultData);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -56,12 +60,19 @@ function CourseListPage(props) {
       </form>
 
       <p className={styles.count}>총 {items.length}개 코스</p>
-
-      <div className={styles.courseList}>
-        {items.map((course) => {
-          return <CourseItem key={course.docId} course={course} />;
-        })}
-      </div>
+      {items.length === 0 && !isLoading ? (
+        <Warn
+          className={styles.emptyList}
+          title="조건에 맞는 코스가 없어요."
+          description="올바른 검색어가 맞는지 다시 한 번 확인해 주세요."
+        />
+      ) : (
+        <div className={styles.courseList}>
+          {items.map((course) => {
+            return <CourseItem key={course.docId} course={course} />;
+          })}
+        </div>
+      )}
     </ListPage>
   );
 }
--- react/09-dw_online_school/src/pages/QuestionListPage.js
+++ react/09-dw_online_school/src/pages/QuestionListPage.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ListPage from "../components/ListPage";
+import styles from "./QuestionListPage.module.css";
+import Input from "../components/Input";
+import searchImg from "../assets/search.svg";
 
+// const [items, setItems] = useState();
 function QuestionListPage(props) {
-  return <ListPage variant="community"></ListPage>;
+  return (
+    <ListPage variant="community">
+      <div className={styles.container}>
+        <Input placeholder="검색으로 질문 찾기" />
+        {/* <p className={styles.count}>총 {items.length}개 코스</p> */}
+        <button>
+          <img src={searchImg} />
+        </button>
+      </div>
+      <div className={styles.box}>
+        <Input />
+        <Input />
+        <Input />
+      </div>
+    </ListPage>
+  );
 }
 
 export default QuestionListPage;
