51bfc42b9c7a0ad75e7fad6ec67a99aab62a74cf
chajoonho <wnsgh882002@gmail.com>
2024-08-08 17:29:04 +0900

    20240808

---
+++ react/14-shop-app/src/firebase.js
@@ -0,0 +1,88 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+  where,
+  getDoc,
+  runTransaction,
+  getDocs,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm_BC-nNwiHwWhLSR4UUIBZS09TJ9AOdc",
+  authDomain: "shop-app-ce659.firebaseapp.com",
+  projectId: "shop-app-ce659",
+  storageBucket: "shop-app-ce659.appspot.com",
+  messagingSenderId: "923610831760",
+  appId: "1:923610831760:web:0258685674d80116ff7115",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+function getCollection(collectionName) {
+  return collection(db, collectionName);
+}
+
+export function getUserAuth() {
+  return auth;
+}
+
+async function getLastNum(collectionName, field) {
+  const q = query(
+    collection(db, collectionName),
+    orderBy(field, "desc"),
+    limit(1)
+  );
+  const lastDoc = await getDocs(q);
+  if (lastDoc.docs.length === 0) {
+    return 0;
+  }
+  const lastNum = lastDoc.docs[0].data()[field];
+  return lastNum;
+}
+
+function getQuery(collectionName, queryOption) {
+  const { conditions = [], orderBys = [], limits } = queryOption;
+  const collect = getCollection(collectionName);
+  let q = query(collect);
+
+  const condition = [
+    { field: "text", operator: "==", value: "test" },
+    { field: "uid", operator: "==", value: "xjdiwjKDJ2jdkxJND2J" },
+  ];
+
+  // where 조건
+  conditions.forEach((condition) => {
+    q = query(q, where(condition.field, condition.operator, condition.value));
+  });
+
+  // orderBy 조건
+  orderBys.forEach((order) => {
+    q = query(q, orderBy(order.field, order.direction || "asc"));
+  });
+
+  // limit 조건
+  q = query(q, limit(limits));
+
+  return q;
+}
+
+export async function getDatas(collectionName, queryOptions) {
+  const q = getQuery(collectionName, queryOptions);
+  const snapshot = await getDocs(q);
+  const docs = snapshot.docs;
+  const resultData = docs.map((doc) => ({ ...doc.data(), docId: doc.id }));
+  return resultData;
+}
